4e38c89196e96a5ccb432c9a3a8e2072704c3afb
Ananya <ananya1474.be20@chitkara.edu.in>
2023-04-18 22:35:11 +0530

    Code

---
+++ server/routes/users.js
@@ -0,0 +1,19 @@
+import express from "express";
+import{
+    getUser,
+    getUserFriends,
+    addRemoveFriends,   
+} from "../controllers/users.js";
+import { verifyToken} from " ../middleware/auth.js";
+import { verufyToken } from "../middleware/auth.js";
+
+const router =express.Router();
+
+//Read
+router.get("/:id",verifyToken,getUser);
+router.get("/:id",verifyToken,getUserFriends);
+
+//Update
+router.patch("/:id:friendId",verifyToken,addRemoveFriends);
+
+export default router;
